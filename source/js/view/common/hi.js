ccb9a4d6e0b49eb30f1227915c60494ddb5b554a
wuyihao <840461198@qq.com>
2017-10-25 16:04:37 +0800

    [update]

--- source/js/view/common/hi.js
+++ source/js/view/common/hi.js
@@ -6,5 +6,22 @@
     return function ($html) {
         console.log($html);
         layer.msg('layer start success!');
-    }
+
+        var origin = location.origin;
+        var url = {
+            test: '/index.html',
+            SingTel: '/index.html#/recharge/SingTel',
+            Starhub: '/index.html#/recharge/Starhub',
+            M1: '/index.html#/recharge/M1',
+            order: '/index.html#/order/order',
+            orderBack: '/index.html#/order/orderBack',
+            policy: '/index.html#/common/policy'
+        };
+
+        $('[data-type="url"]').each(function () {
+            var $this = $(this);
+            $this.attr('href', origin + url[$this.data('key')]);
+            $this.attr('target', '_blank');
+        });
+    };
 });
