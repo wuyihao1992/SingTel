3c2b2b5fcdecd6a89306f468100bd61646a2217a
wuyihao <840461198@qq.com>
2017-10-30 22:27:29 +0800

    m

--- source/js/view/common/hi.js
+++ source/js/view/common/hi.js
@@ -24,8 +24,8 @@
             var $this = $(this);
             var data = $this.data(), url = '';
 
-            // url = origin + '/index.html#' + urlObj[data.key];
-            url = '../../../../index.html#' + urlObj[data.key];
+            url = origin + location.pathname + '#' + urlObj[data.key];
+            // url = '../../../../index.html#' + urlObj[data.key];
             if (data.key == 'recharge') {
                 url += '?type=' + data.url;
             }
