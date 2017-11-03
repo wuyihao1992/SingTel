3931f721013ce1a8b74b8559fb61cb726f23627f
wuyihao <840461198@qq.com>
2017-11-03 19:24:49 +0800

    m

--- source/js/view/recharge/M1.js
+++ source/js/view/recharge/M1.js
@@ -9,9 +9,22 @@
 
         var Charge = function () {
             this.fetchData = function () {
-                var tips = '• 价格: ￥111.60\r\n\r\n• 【红利余额不能开通数据计划】\r\n\r\n• 有效期：50天\r\n\r\n• 红利账户：$12\r\n\r\n• 附加账户：\r\n\r\n• ➢本地话费：120分钟\r\n\r\n• 本地短信/流量：500条/350MB\r\n\r\n• 免费接听: 50天拨打 #100*2# 回复对应数字查询MCard$23计划余额';
-                tips = tips.replace(/\r\n\r\n/gi, '<br/>');
-                $('.r-tips').html(tips);
+                api({class_type: 'M1'}, {url: 'api/item_list'}).then(function (result) {
+                    if (result.status == 0){
+                        var data = result.data;
+
+                        if (!!data) {
+                            var resultList = fun.itemList(data);
+                            $('.r-tab__ul', $html).html(resultList.tabStr);
+                            $('.r-tabContent > ul', $html).html(resultList.contStr);
+                        }else {
+                            fun.swal('暂无套餐列表', 'error');
+                        }
+                    }
+
+                }, function () {
+                    fun.swal('请求失败，请稍后重试', 'error');
+                });
             };
 
             this.init = function () {
