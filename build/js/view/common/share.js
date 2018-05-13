5f3c47d72c041d0ceb70c1f973687b2d8669b55f
wuyihao <840461198@qq.com>
2018-05-13 15:12:09 +0800

    [update] member

--- build/js/view/common/share.js
+++ build/js/view/common/share.js
@@ -14,8 +14,21 @@
 
             });*/
 
-            var tips = '';  // FIXME
+            var tips = '';
+
+            // 获取积分的方式
+            api({}, {type: 'GET', url: '/api/text?text_name=credit_rule'}).then(function (result) {
+                if (!!result && result.status == 0) {
+                    tips= fun.formTips(result.data);
+                }
+            });
+
             $('#showDetail', $html).on('click', function (e) {
+                if (!tips) {
+                    fun.swal('正在获取积分规则，请稍后！', 'info');
+                    return false;
+                }
+
                 layer.open({
                     title: false,
                     btn: false,
